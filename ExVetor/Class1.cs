c66bd82c59c32c7966f7e13d603af66ca7fb0c0b
Lucas Denipoti <lucasfdenipoti@gmail.com>
2020-10-28 22:08:02 -0300

    início do código

---
+++ ExVetor/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExVetor
+{
+    class Produto
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+    }
+}
--- ExVetor/Program.cs
+++ ExVetor/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace ExVetor
 {
@@ -6,7 +7,21 @@ namespace ExVetor
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
+            int n = int.Parse(Console.ReadLine()); //Criando a variável de entrada
+            Produto[] vect = new Produto[n]; //Criando vetor com o número da variável de entrada
+            for(int i = 0; i < n; i++) //Utilização do comando for para percorrer o vetor
+            {
+                string name = Console.ReadLine();
+                double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                //Entrada dos elementos no vetor pelo usuário
+                vect[i] = new Produto { Name = name, Price = price };
+            }
+            double soma = 0.0; //Variável soma    
+            for(int i= 0; i < n; i++) //Utilização do for para percorrer o vetor
+            {
+                soma += vect[i].Price; //Somatoriados elementos inseridos
+            }
+            Console.ReadKey();
+        } 
     }
 }
